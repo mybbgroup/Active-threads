91789dc613fd305e63061091d473b81c27e45e57
Laird Shaw <lairdshaw77@gmail.com>
2020-02-02 01:40:34 +1000

    Bumped version to 1.2.1 so that auto-upgrades for recent changes work.

--- root/inc/plugins/activethreads.php
+++ root/inc/plugins/activethreads.php
@@ -32,10 +32,10 @@ function activethreads_info() {
 		'website'       => '',
 		'author'        => 'Laird Shaw',
 		'authorsite'    => '',
-		'version'       => '1.2.0-dev',
+		'version'       => '1.2.1',
 		// Constructed by converting each digit of 'version' above into two digits (zero-padded if necessary),
 		// then concatenating them, then removing any leading zero(es) to avoid the value being interpreted as octal.
-		'version_code'  => '10200',
+		'version_code'  => '10201',
 		'guid'          => '',
 		'codename'      => C_ACT,
 		'compatibility' => '18*'
