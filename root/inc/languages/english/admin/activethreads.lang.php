0b3392dffc0d580e0e6ec27ee124929743109bfb
Laird Shaw <lairdshaw77@gmail.com>
2020-02-01 23:34:17 +1000

    Improved the plugin's description

--- root/inc/languages/english/admin/activethreads.lang.php
+++ root/inc/languages/english/admin/activethreads.lang.php
@@ -1,6 +1,6 @@
 <?php
 
 $l['act_name'] = 'Active Threads';
-$l['act_desc'] = 'Shows the most active threads by number of posts';
+$l['act_desc'] = 'Shows the threads that were active (posted to at least once) during a given period.';
 $l['act_max_interval_in_secs_title'] = 'Maximum allowed interval (seconds)';
 $l['act_max_interval_in_secs_desc' ] = 'Queries over a large interval can be expensive. Set the maximum interval in seconds here. Zero for unlimited.';
